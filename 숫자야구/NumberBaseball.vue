81998296354010339284f71c48675128d3174f57
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-19 08:47:12 +0900

    숫자야구 working on checkNums

--- 숫자야구/NumberBaseball.vue
+++ 숫자야구/NumberBaseball.vue
@@ -17,11 +17,54 @@ export default {
   name: 'NumberBaseball',
   data() {
     return {
+      randomNum: [],
       inputNum: '',
+      retObj: {
+        input: '',
+        strike: 0,
+        ball: 0,
+      },
+      tries: [],
     };
   },
   methods: {
     init() {
+      this.checkInputVal();
+      this.genRandomNum();
+      console.log(this.randomNum);
+    },
+    onSubmitForm(e) {
+      e.preventDefault();
+
+      this.retObj.input = this.inputNum;
+
+      for (const i in this.randomNum) {
+        if (+this.inputNum[i] === this.randomNum[i]) {
+          this.retObj.strike++;
+        } else if (
+          +this.inputNum[i] !== this.randomNum[i] &&
+          this.randomNum.includes(+this.inputNum[i])
+        ) {
+          this.retObj.ball++;
+        }
+      }
+      console.log(this.retObj);
+      this.tries.push(this.retObj);
+
+      this.retObj.strike = 0;
+      this.retObj.ball = 0;
+      console.log(this.tries);
+    },
+    genRandomNum() {
+      while (true) {
+        if (this.randomNum.length === 4) return;
+        const num = Math.floor(Math.random() * 9 + 1);
+        if (!this.randomNum.includes(num)) {
+          this.randomNum.push(num);
+        }
+      }
+    },
+    checkInputVal() {
       const input = document.querySelector('.input');
       const btn = document.querySelector('.btn');
       input.addEventListener('keyup', () => {
@@ -31,9 +74,6 @@ export default {
           btn.disabled = false;
         }
       });
-    },
-    onSubmitForm(e) {
-      e.preventDefault();
     },
   },
   mounted() {
